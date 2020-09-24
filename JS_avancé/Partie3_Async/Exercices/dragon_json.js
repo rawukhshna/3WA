db576592f923bb6db394e3c91845292a6b8b14a7
Roxane <roxane-girma@hotmail.fr>
2020-09-24 16:13:56 +0200

    dragon_json.js

--- JS_avancé/Partie3_Async/Exercices/dragon_json.js
+++ JS_avancé/Partie3_Async/Exercices/dragon_json.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 
-const dragon = (file) => (new Promise((resolve, reject) => {
+// 1 ) 
+const dragons = (file) => (new Promise((resolve, reject) => {
     fs.readFile( file, { encoding: 'utf8' }, (err, data) => {
         if (err) {
             reject("File read failed:", err);
@@ -10,6 +11,28 @@ const dragon = (file) => (new Promise((resolve, reject) => {
     });
 }));
 
-dragon('./data/dragons.json')
+dragons('./data/dragons.json')
     .then(data => console.log('File data:', JSON.parse(data)) )
+    .catch(err => console.log( err));
+
+// 2 )
+
+dragons('./data/dragons.json')
+    .then(data => {
+        const dragonsList = JSON.parse(data);
+ 
+        const dragonsDSC = dragonsList.dragons.sort((a,b) => b.age - a.age)
+    
+        const youngestDragon = dragonsDSC[dragonsDSC.length -1]
+
+        console.log(`Le plus vieux dragon est ${oldestDragon.name}(${oldestDragon.age} ans).`)
+// 3
+        const oldestDragon = dragonsDSC[0]
+    
+        console.log(`Le plus jeune dragon est ${youngestDragon.name} (${youngestDragon.age} ans).`)
+
+// 4
+        console.log(dragonsDSC);
+        
+    })
     .catch(err => console.log( err));
